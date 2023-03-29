a7920f768533cc50e9b517754cb662a90d3da3b7
Mauricio Gabriel Garcia Catellan <mauricio.catellan@autolab.corp>
2023-03-29 08:44:00 -0300

    Dev - Função de estimação dos parâmetros funcional

--- DataProcessing/limitorderbook_process.py
+++ DataProcessing/limitorderbook_process.py
@@ -8,20 +8,24 @@ def calculate_upanddown_mean(df: pd.DataFrame):
     count_upevent = 0
     ask_downevent_mean = 0
     count_downevent = 0
+    t_up_event = []
+    t_down_event = []
     for index, name in df.items():
         if index == 0:
             continue
         if(df[index] > df[index - 1]):
             count_upevent = count_upevent + 1
+            t_up_event.append(index)
             ask_upevent_mean = ask_upevent_mean + (df[index] - df[index - 1])
         else: 
             count_downevent = count_downevent + 1
+            t_down_event.append(index)
             ask_downevent_mean = ask_downevent_mean + (df[index - 1] - df[index])
 
 
     ask_upevent_mean = ask_upevent_mean/count_upevent
     ask_downevent_mean = ask_downevent_mean/count_downevent
-    return [ask_upevent_mean, ask_downevent_mean]
+    return [ask_upevent_mean, ask_downevent_mean, t_up_event, t_down_event]
 
 
 dec_test = np.loadtxt('./Test_Dst_NoAuction_DecPre_CF_9.txt')
@@ -33,10 +37,10 @@ df = pd.DataFrame(dfnp)
 #print(df.head())
 
 dfAskPricesLv1 = df.loc[:, 0]
-dfAskVolumesLv1 = df.loc[:MEAN_NUM_ROWS, 1]
+dfAskVolumesLv1 = df.loc[:MEAN_NUM_ROWS - 1, 1]
 
 dfBidPricesLv1 = df.loc[:, 2]
-dfBidVolumesLv1 = df.loc[:MEAN_NUM_ROWS, 3]
+dfBidVolumesLv1 = df.loc[:MEAN_NUM_ROWS - 1, 3]
 
 #print(dfAskPricesLv1.head())
 #print(dfAskVolumesLv1.head())
@@ -46,8 +50,8 @@ dfBidVolumesLv1 = df.loc[:MEAN_NUM_ROWS, 3]
 dfAskVolumesLv1.reset_index()
 dfBidVolumesLv1.reset_index()
 
-[ask_upevent_mean, ask_downevent_mean] = calculate_upanddown_mean(dfAskVolumesLv1)
-[bid_upevent_mean, bid_downevent_mean] = calculate_upanddown_mean(dfBidVolumesLv1)
+[ask_upevent_mean, ask_downevent_mean, ask_t_upevent, ask_t_downevent] = calculate_upanddown_mean(dfAskVolumesLv1)
+[bid_upevent_mean, bid_downevent_mean, bid_t_upevent, bid_t_downevent] = calculate_upanddown_mean(dfBidVolumesLv1)
 
 print("Média dos Volumes de Ask de Nível 1")
 print("Eventos de subida: ")
@@ -60,3 +64,15 @@ print("Eventos de subida: ")
 print(bid_upevent_mean)
 print("Eventos de descida: ")
 print(bid_downevent_mean)
+
+print("Janela de tempo (T):")
+print(dfAskVolumesLv1.__len__)
+
+print("Tempos de subida do Ask:")
+print(ask_t_upevent)
+print("Tempos de descida do Ask:")
+print(ask_t_downevent)
+print("Tempos de subida do Bid:")
+print(bid_t_upevent)
+print("Tempos de descida do Bid:")
+print(bid_t_downevent)
--- DataProcessing/parameter_estimation.py
+++ DataProcessing/parameter_estimation.py
@@ -8,8 +8,16 @@ import math
 
 def Rmn(k, betamn, tm, tn):
     parc2R = 0
-    for i in range(k-1, k):
+    i = 0
+
+    for index, t in enumerate(tn):
+        if(t >= tm[k-1]):
+            i = index
+            break
+
+    while(i < len(tn) and tn[i] >= tm[k-1] and tn[i] < tm[k]):
         parc2R = parc2R + math.exp(-betamn*(tm[k] - tn[i]))
+        i = i + 1
     
     if(k == 0):
         return 0
@@ -21,14 +29,14 @@ def sumParc2(alfam, betam, t, T):
     for n in range(4):
         parct = 0
         tn = t[n]
-        for k in range(tn):
+        for k in range(len(tn)):
             parct = parct + 1 - math.exp(-betam[n]*(T - tn[k]))
         parc2 = parc2 + alfam[n]/betam[n]*parct
     return parc2
 
 def sumParc3(mum, t, tm, alfam, betam):
     parc3 = 0
-    for k in range(tm):
+    for k in range(len(tm)):
         recparc = 0
         for n in range(4):
             tn = t[n]
@@ -40,19 +48,33 @@ def sumParc3(mum, t, tm, alfam, betam):
 def func (teta):
     [mu, alfa, beta] = teta
     
-    T = 30
-    t = [[], [], [], []]    #4 eventos, cada um com seus tk
-
+    #Baseado em limitorderbook_process.py
+    T = 60                  
+    t = [[1, 3, 5, 7, 9, 10, 11, 12, 14, 15, 18, 19, 20, 21, 22, 23, 24, 25, 27, 29, 32, 33, 35, 38, 40, 41,    #Descida Bid
+           43, 44, 45, 46, 47, 48, 49, 50, 52, 53, 54, 56, 57, 58, 59],
+            [2, 4, 6, 8, 13, 16, 17, 26, 28, 30, 31, 34, 36, 37, 39, 42, 51, 55],                             #Subida Bid
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,    #Descida Ask
+          31, 32, 33, 35, 37, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 52, 54, 55, 56, 58, 59],
+          [11, 14, 18, 34, 36, 38, 43, 53, 57]]                                                               #Subida Ask
+                                                                                                             
     res = 0
     for m in range(4):
         tm = t[m]
 
-        parc1 = -mu[m]*T 
+        parc1 = -1*mu[m]*T 
         parc2 = -1*sumParc2(alfa[m], beta[m], t, T)
         parc3 = sumParc3(mu[m], t, tm, alfa[m], beta[m])
         
         res = res + parc1 + parc2 + parc3
 
     return -1*res
+
+
+#Valores de teste
+mu = [0.08, 0.08, 0.05, 0.05]
+alfa = [[0 , 0.4, 0, 0],[0.4, 0,0,0],[0,0, 0.5, 0.3],[0,0,0.3,0.5]]
+beta= [[0.6,0.6,0.6,0.6],[0.6,0.6,0.6,0.6],[1.2,1.2,1.2,1.2],[1.2,1.2,1.2,1.2]]
+
+print(func([mu, alfa, beta]))
 
 #Minimizar func
